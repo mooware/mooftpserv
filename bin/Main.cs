93c4d41001db0165a3e913931923db812d02a5ad
Markus Pointner <markus.pointner@mooware.at>
2013-04-23 00:53:18 +0200

    extra setup for WinCE

--- bin/Main.cs
+++ bin/Main.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Sockets;
 using System.IO;
 using mooftpserv;
 
@@ -14,12 +15,12 @@ namespace mooftpserv
 
             int port = 2121;
             try {
+                // CF is missing int.TryParse
                 if (argList.Count > 0) {
                     port = int.Parse(argList[0]);
                     argList.RemoveAt(0);
                 }
             } catch (Exception) {
-                // CF is missing int.TryParse
             }
 
             bool verbose = false;
@@ -31,7 +32,12 @@ namespace mooftpserv
                 }
             }
 
-            DirectoryInfo startDir = new DirectoryInfo(Path.GetFullPath("."));
+#if WindowsCE
+            // WinCE has no "current directory"
+            startDir = new DirectoryInfo(Path.GetFullPath(@"\\"));
+#else
+            startDir = new DirectoryInfo(Directory.GetCurrentDirectory());
+#endif
             try {
                 if (argList.Count > 0) {
                   startDir = new DirectoryInfo(Path.GetFullPath(argList[0]));
@@ -41,28 +47,33 @@ namespace mooftpserv
                 Console.Error.WriteLine(ex.Message);
             }
 
+#if WindowsCE
             // on WinCE, 0.0.0.0 does not work because for accepted sockets,
             // LocalEndPoint would also say 0.0.0.0 instead of the real IP
             IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress bindIp = IPAddress.Any;
+            IPAddress bindIp = IPAddress.Loopback;
             foreach (IPAddress ip in host.AddressList) {
-                if (!IPAddress.IsLoopback(ip)) {
+                if (ip.AddressFamily != AddressFamily.InterNetworkV6 && !IPAddress.IsLoopback(ip)) {
                     bindIp = ip;
                     break;
                 }
             }
+#else
+            IPAddress bindIp = IPAddress.Any;
+#endif
 
             Server srv = new Server(bindIp, port,
                                     new DefaultAuthHandler(),
                                     new DefaultFileSystemHandler(startDir),
                                     new DefaultLogHandler(verbose));
 
-            Console.Out.WriteLine("Starting server on {0}:{1}, hosting {2}", bindIp, port, startDir);
+            Console.Out.WriteLine("Starting server on {0}:{1}, default path is {2}", bindIp, port, startDir);
 
             try {
                 srv.Run();
             } catch (Exception ex) {
                 Console.Error.WriteLine(ex.Message);
+                srv.Stop();
             }
         }
     }
