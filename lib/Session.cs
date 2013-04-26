59453deec5cce8f71da32aec662be50e5a018c74
Markus Pointner <markus.pointner@mooware.at>
2013-04-26 22:10:16 +0200

    cleanup, comments

--- lib/Session.cs
+++ lib/Session.cs
@@ -9,6 +9,7 @@ namespace mooftpserv
 {
     public class Session
     {
+        // transfer data type, ascii or binary
         enum DataType { ASCII, IMAGE };
 
         // size of stream buffers
@@ -20,7 +21,7 @@ namespace mooftpserv
         // response text for initial response. preceeded by application name and version number.
         private static string[] HELLO_TEXT = { "What can I do for you?", "Good day, sir or madam.", "Hey ho let's go!", "The poor man's FTP server." };
         // response text for general ok messages
-        private static string[] OK_TEXT = { "Sounds good.", "Barely acceptable.", "Alright, I'll do it...", "Consider it done." };
+        private static string[] OK_TEXT = { "Sounds good.", "A 'thank you' wouldn't hurt.", "Alright, I'll do it...", "Consider it done." };
         // Result for FEAT command
         private static string[] FEATURES = { "MDTM", "PASV", "SIZE", "UTF8" };
 
@@ -750,6 +751,7 @@ namespace mooftpserv
 
         private string EscapePath(string path)
         {
+            // double-quotes in paths are escaped by doubling them
             return '"' + path.Replace("\"", "\"\"") + '"';
         }
 
